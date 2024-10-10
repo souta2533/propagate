b82d3576e55453e3d212e198ed992362fa5358f0
souta <souta.y61233@gmail.com>
2024-10-10 16:52:15 +0900

    スタイルの変更

--- frontend/hooks/useAggregatedData.js
+++ frontend/hooks/useAggregatedData.js
@@ -1,52 +1,57 @@
-import { useQuery } from '@tanstack/react-query';
-import { fetchAggregatedDataFromDashboard } from '../lib/getData';
-
+import { useQuery } from "@tanstack/react-query";
+import { fetchAggregatedDataFromDashboard } from "../lib/getData";
 
 // dashboardで使用されるデータを集計
-const fetchAggregatedData = async (session, propertyIds, startDate, endDate) => {
-    if (
-        !session ||
-        !propertyIds ||
-        !startDate ||
-        !endDate
-    ) {
-        console.warn("Session, propertyIds, startDate, or endDate is null");
-        return;
-    }
+const fetchAggregatedData = async (
+  session,
+  propertyIds,
+  startDate,
+  endDate
+) => {
+  if (!session || !propertyIds || !startDate || !endDate) {
+    console.warn("Session, propertyIds, startDate, or endDate is null");
+    return;
+  }
+  console.log("SS:", startDate);
+  const jwtToken = session.access_token;
+  if (!jwtToken) {
+    console.warn("JWT Token is null");
+    return;
+  }
 
-    const jwtToken = session.access_token;
-    if (!jwtToken) {
-        console.warn("JWT Token is null");
-        return;
-    }
+  const aggregatedDataByPropertyId = {};
 
-    const aggregatedDataByPropertyId = {};
+  for (const property of propertyIds) {
+    const propertyId = property.properties_id;
+    try {
+      const aggregatedData = await fetchAggregatedDataFromDashboard(
+        jwtToken,
+        propertyId,
+        startDate,
+        endDate
+      );
 
-      for (const property of propertyIds) {
-        const propertyId = property.properties_id;
-        try {
-          const aggregatedData = await fetchAggregatedDataFromDashboard(
-            jwtToken,
-            propertyId,
-            startDate,
-            endDate
-          );
-
-          if (aggregatedData) {
-            aggregatedDataByPropertyId[propertyId] = aggregatedData;
-          }
-        } catch (error) {
-          console.error("Error fetching aggregated data:", error);
-        }
+      if (aggregatedData) {
+        aggregatedDataByPropertyId[propertyId] = aggregatedData;
       }
-    
-    return aggregatedDataByPropertyId;
+    } catch (error) {
+      console.error("Error fetching aggregated data:", error);
+    }
+  }
+
+  return aggregatedDataByPropertyId;
 };
 
 export const useAggregatedData = (session, propertyIds, startDate, endDate) => {
-    return useQuery({
-        queryKey: ['aggregatedData'],
-        queryFn: () => fetchAggregatedData(session, propertyIds, startDate, endDate),
-        enabled: !!session && !!propertyIds && propertyIds.length > 1 && !!startDate && !!endDate,
-    });
+  return useQuery({
+    queryKey: ["aggregatedData"],
+    queryFn: () =>
+      fetchAggregatedData(session, propertyIds, startDate, endDate),
+    enabled:
+      !!session &&
+      !!propertyIds &&
+      propertyIds.length > 1 &&
+      !!startDate &&
+      !!endDate,
+  });
 };
--- frontend/pages/dashboard.js
+++ frontend/pages/dashboard.js
@@ -23,7 +23,7 @@ import PercentageTable from "../components/graph/PercentageTable";
 import Table from "../components/graph/Table";
 import Table2 from "../components/graph/Table2";
 import WebMetricsChart from "../components/graph/WebMetricsChart";
-import Trialgle from "../components/graph/Trialgle";
+import TriangleChart from "../components/graph/TriangleChart";
 import "../styles/dashboard.css";
 
 //Selectのカスタム
@@ -80,148 +80,6 @@ const customStyles = {
 };
 
 const Dashboard = () => {
-  const sampledata1 = [
-    { date: "2024-05-15", PV: 1922, CV: 52, CVR: 2.71, UU: 1220 },
-    { date: "2024-05-16", PV: 708, CV: 59, CVR: 8.33, UU: 1172 },
-    { date: "2024-05-17", PV: 1798, CV: 63, CVR: 3.5, UU: 1104 },
-    { date: "2024-05-18", PV: 690, CV: 108, CVR: 15.65, UU: 1164 },
-    { date: "2024-05-19", PV: 1464, CV: 97, CVR: 6.63, UU: 532 },
-    { date: "2024-05-20", PV: 1548, CV: 115, CVR: 7.43, UU: 1107 },
-    { date: "2024-05-21", PV: 867, CV: 140, CVR: 16.15, UU: 1049 },
-    { date: "2024-05-22", PV: 793, CV: 191, CVR: 24.09, UU: 1206 },
-    { date: "2024-05-23", PV: 1141, CV: 143, CVR: 12.53, UU: 758 },
-    { date: "2024-05-24", PV: 573, CV: 191, CVR: 33.33, UU: 1203 },
-    { date: "2024-05-25", PV: 1498, CV: 132, CVR: 8.81, UU: 885 },
-    { date: "2024-05-26", PV: 1819, CV: 61, CVR: 3.35, UU: 1072 },
-    { date: "2024-05-27", PV: 1106, CV: 145, CVR: 13.11, UU: 1000 },
-    { date: "2024-05-28", PV: 893, CV: 158, CVR: 17.69, UU: 1496 },
-    { date: "2024-05-29", PV: 852, CV: 188, CVR: 22.07, UU: 1131 },
-    { date: "2024-05-30", PV: 1979, CV: 164, CVR: 8.29, UU: 1499 },
-    { date: "2024-05-31", PV: 1785, CV: 91, CVR: 5.1, UU: 1035 },
-    { date: "2024-06-01", PV: 1242, CV: 108, CVR: 8.7, UU: 1350 },
-    { date: "2024-06-02", PV: 902, CV: 168, CVR: 18.63, UU: 1183 },
-    { date: "2024-06-03", PV: 1766, CV: 175, CVR: 9.91, UU: 873 },
-    { date: "2024-06-04", PV: 1751, CV: 126, CVR: 7.2, UU: 521 },
-    { date: "2024-06-05", PV: 516, CV: 135, CVR: 26.16, UU: 808 },
-    { date: "2024-06-06", PV: 766, CV: 193, CVR: 25.2, UU: 1304 },
-    { date: "2024-06-07", PV: 1650, CV: 67, CVR: 4.06, UU: 991 },
-    { date: "2024-06-08", PV: 1842, CV: 181, CVR: 9.83, UU: 1460 },
-    { date: "2024-06-09", PV: 1025, CV: 114, CVR: 11.12, UU: 631 },
-    { date: "2024-06-10", PV: 884, CV: 51, CVR: 5.77, UU: 1104 },
-    { date: "2024-06-11", PV: 1088, CV: 147, CVR: 13.51, UU: 560 },
-    { date: "2024-06-12", PV: 1943, CV: 55, CVR: 2.83, UU: 594 },
-    { date: "2024-06-13", PV: 822, CV: 177, CVR: 21.53, UU: 1233 },
-    { date: "2024-06-14", PV: 1249, CV: 80, CVR: 6.41, UU: 1425 },
-    { date: "2024-06-15", PV: 1417, CV: 93, CVR: 6.56, UU: 585 },
-    { date: "2024-06-16", PV: 1601, CV: 102, CVR: 6.37, UU: 1219 },
-    { date: "2024-06-17", PV: 1845, CV: 58, CVR: 3.14, UU: 549 },
-    { date: "2024-06-18", PV: 1282, CV: 160, CVR: 12.48, UU: 1466 },
-    { date: "2024-06-19", PV: 890, CV: 108, CVR: 12.13, UU: 1115 },
-    { date: "2024-06-20", PV: 757, CV: 175, CVR: 23.12, UU: 1295 },
-    { date: "2024-06-21", PV: 1830, CV: 68, CVR: 3.72, UU: 907 },
-    { date: "2024-06-22", PV: 1832, CV: 172, CVR: 9.39, UU: 1304 },
-    { date: "2024-06-23", PV: 611, CV: 148, CVR: 24.22, UU: 1205 },
-    { date: "2024-06-24", PV: 725, CV: 99, CVR: 13.66, UU: 1493 },
-    { date: "2024-06-25", PV: 741, CV: 144, CVR: 19.43, UU: 1078 },
-    { date: "2024-06-26", PV: 798, CV: 99, CVR: 12.41, UU: 646 },
-    { date: "2024-06-27", PV: 1634, CV: 93, CVR: 5.69, UU: 716 },
-    { date: "2024-06-28", PV: 721, CV: 154, CVR: 21.36, UU: 660 },
-    { date: "2024-06-29", PV: 1701, CV: 136, CVR: 8.0, UU: 936 },
-    { date: "2024-06-30", PV: 1923, CV: 95, CVR: 4.94, UU: 838 },
-    { date: "2024-07-01", PV: 1490, CV: 167, CVR: 11.21, UU: 1286 },
-    { date: "2024-07-02", PV: 1952, CV: 52, CVR: 2.66, UU: 641 },
-    { date: "2024-07-03", PV: 728, CV: 119, CVR: 16.35, UU: 772 },
-    { date: "2024-07-04", PV: 1436, CV: 162, CVR: 11.28, UU: 1207 },
-    { date: "2024-07-05", PV: 1301, CV: 59, CVR: 4.53, UU: 1028 },
-    { date: "2024-07-06", PV: 1088, CV: 58, CVR: 5.33, UU: 922 },
-    { date: "2024-07-07", PV: 1730, CV: 177, CVR: 10.23, UU: 1218 },
-    { date: "2024-07-08", PV: 1808, CV: 78, CVR: 4.31, UU: 1205 },
-    { date: "2024-07-09", PV: 1380, CV: 144, CVR: 10.43, UU: 1133 },
-    { date: "2024-07-10", PV: 1353, CV: 172, CVR: 12.71, UU: 1108 },
-    { date: "2024-07-11", PV: 1344, CV: 186, CVR: 13.84, UU: 1097 },
-    { date: "2024-07-12", PV: 793, CV: 150, CVR: 18.92, UU: 1474 },
-    { date: "2024-07-13", PV: 742, CV: 78, CVR: 10.51, UU: 945 },
-    { date: "2024-07-14", PV: 1431, CV: 101, CVR: 7.06, UU: 501 },
-    { date: "2024-07-15", PV: 1238, CV: 196, CVR: 15.83, UU: 823 },
-    { date: "2024-07-16", PV: 1487, CV: 72, CVR: 4.84, UU: 1231 },
-    { date: "2024-07-17", PV: 1099, CV: 80, CVR: 7.28, UU: 986 },
-    { date: "2024-07-18", PV: 1761, CV: 144, CVR: 8.18, UU: 1236 },
-    { date: "2024-07-19", PV: 1485, CV: 83, CVR: 5.59, UU: 963 },
-    { date: "2024-07-20", PV: 901, CV: 178, CVR: 19.76, UU: 1161 },
-    { date: "2024-07-21", PV: 1356, CV: 86, CVR: 6.34, UU: 1374 },
-    { date: "2024-07-22", PV: 979, CV: 197, CVR: 20.12, UU: 1133 },
-    { date: "2024-07-23", PV: 1856, CV: 189, CVR: 10.18, UU: 860 },
-    { date: "2024-07-24", PV: 1667, CV: 172, CVR: 10.32, UU: 902 },
-    { date: "2024-07-25", PV: 740, CV: 58, CVR: 7.84, UU: 854 },
-    { date: "2024-07-26", PV: 1421, CV: 59, CVR: 4.15, UU: 1372 },
-    { date: "2024-07-27", PV: 1153, CV: 148, CVR: 12.84, UU: 716 },
-    { date: "2024-07-28", PV: 1535, CV: 68, CVR: 4.43, UU: 658 },
-    { date: "2024-07-29", PV: 562, CV: 86, CVR: 15.3, UU: 853 },
-    { date: "2024-07-30", PV: 1440, CV: 191, CVR: 13.26, UU: 1022 },
-    { date: "2024-07-31", PV: 1388, CV: 193, CVR: 13.9, UU: 692 },
-    { date: "2024-08-01", PV: 1352, CV: 102, CVR: 7.54, UU: 1310 },
-    { date: "2024-08-02", PV: 1333, CV: 131, CVR: 9.83, UU: 817 },
-    { date: "2024-08-03", PV: 1456, CV: 89, CVR: 6.11, UU: 1045 },
-    { date: "2024-08-04", PV: 1324, CV: 157, CVR: 11.86, UU: 1320 },
-    { date: "2024-08-05", PV: 1789, CV: 102, CVR: 5.7, UU: 1567 },
-    { date: "2024-08-06", PV: 1543, CV: 76, CVR: 4.92, UU: 1389 },
-    { date: "2024-08-07", PV: 1620, CV: 145, CVR: 8.95, UU: 1298 },
-    { date: "2024-08-08", PV: 1398, CV: 112, CVR: 8.01, UU: 1187 },
-    { date: "2024-08-09", PV: 1487, CV: 98, CVR: 6.59, UU: 1254 },
-    { date: "2024-08-10", PV: 1734, CV: 123, CVR: 7.09, UU: 1432 },
-    { date: "2024-08-11", PV: 1599, CV: 110, CVR: 6.88, UU: 1365 },
-    { date: "2024-08-12", PV: 1478, CV: 134, CVR: 9.07, UU: 1278 },
-    { date: "2024-08-13", PV: 1345, CV: 89, CVR: 6.62, UU: 1156 },
-    { date: "2024-08-14", PV: 1689, CV: 145, CVR: 8.59, UU: 1423 },
-    { date: "2024-08-15", PV: 1534, CV: 102, CVR: 6.65, UU: 1304 },
-    { date: "2024-08-16", PV: 1456, CV: 78, CVR: 5.36, UU: 1234 },
-    { date: "2024-08-17", PV: 1723, CV: 134, CVR: 7.78, UU: 1489 },
-    { date: "2024-08-18", PV: 1589, CV: 112, CVR: 7.05, UU: 1378 },
-    { date: "2024-08-19", PV: 1498, CV: 98, CVR: 6.54, UU: 1267 },
-    { date: "2024-08-20", PV: 1678, CV: 123, CVR: 7.33, UU: 1456 },
-    { date: "2024-08-21", PV: 1543, CV: 110, CVR: 7.13, UU: 1345 },
-    { date: "2024-08-22", PV: 1398, CV: 134, CVR: 9.59, UU: 1234 },
-    { date: "2024-08-23", PV: 1487, CV: 89, CVR: 5.98, UU: 1123 },
-    { date: "2024-08-24", PV: 1734, CV: 145, CVR: 8.36, UU: 1432 },
-    { date: "2024-08-25", PV: 1599, CV: 102, CVR: 6.38, UU: 1321 },
-    { date: "2024-08-26", PV: 1478, CV: 76, CVR: 5.14, UU: 1210 },
-    { date: "2024-08-27", PV: 1345, CV: 123, CVR: 9.14, UU: 1456 },
-    { date: "2024-08-28", PV: 1689, CV: 110, CVR: 6.51, UU: 1345 },
-    { date: "2024-08-29", PV: 1534, CV: 134, CVR: 8.74, UU: 1234 },
-    { date: "2024-08-30", PV: 1456, CV: 89, CVR: 6.11, UU: 1123 },
-    { date: "2024-08-31", PV: 1723, CV: 145, CVR: 8.41, UU: 1432 },
-    { date: "2024-09-01", PV: 1589, CV: 102, CVR: 6.42, UU: 1321 },
-    { date: "2024-09-02", PV: 1498, CV: 76, CVR: 5.07, UU: 1210 },
-    { date: "2024-09-03", PV: 1678, CV: 123, CVR: 7.33, UU: 1456 },
-    { date: "2024-09-04", PV: 1543, CV: 110, CVR: 7.13, UU: 1345 },
-    { date: "2024-09-05", PV: 1398, CV: 134, CVR: 9.59, UU: 1234 },
-    { date: "2024-09-06", PV: 1487, CV: 89, CVR: 5.98, UU: 1123 },
-    { date: "2024-09-07", PV: 1734, CV: 145, CVR: 8.36, UU: 1432 },
-    { date: "2024-09-08", PV: 1599, CV: 102, CVR: 6.38, UU: 1321 },
-    { date: "2024-09-09", PV: 1478, CV: 76, CVR: 5.14, UU: 1210 },
-    { date: "2024-09-10", PV: 1345, CV: 123, CVR: 9.14, UU: 1456 },
-    { date: "2024-09-11", PV: 1689, CV: 110, CVR: 6.51, UU: 1345 },
-    { date: "2024-09-12", PV: 1534, CV: 134, CVR: 8.74, UU: 1234 },
-    { date: "2024-09-13", PV: 1456, CV: 89, CVR: 6.11, UU: 1123 },
-    { date: "2024-09-14", PV: 1723, CV: 145, CVR: 8.41, UU: 1432 },
-    { date: "2024-09-15", PV: 1589, CV: 102, CVR: 6.42, UU: 1321 },
-    { date: "2024-09-16", PV: 1498, CV: 76, CVR: 5.07, UU: 1210 },
-    { date: "2024-09-17", PV: 1678, CV: 123, CVR: 7.33, UU: 1456 },
-    { date: "2024-09-18", PV: 1543, CV: 110, CVR: 7.13, UU: 1345 },
-    { date: "2024-09-19", PV: 1398, CV: 134, CVR: 9.59, UU: 1234 },
-    { date: "2024-09-20", PV: 1487, CV: 89, CVR: 5.98, UU: 1123 },
-    { date: "2024-09-21", PV: 1734, CV: 145, CVR: 8.36, UU: 1432 },
-    { date: "2024-09-22", PV: 1599, CV: 102, CVR: 6.38, UU: 1321 },
-    { date: "2024-09-23", PV: 1478, CV: 76, CVR: 5.14, UU: 1210 },
-    { date: "2024-09-24", PV: 1345, CV: 123, CVR: 9.14, UU: 1456 },
-    { date: "2024-09-25", PV: 1689, CV: 110, CVR: 6.51, UU: 1345 },
-    { date: "2024-09-26", PV: 1534, CV: 134, CVR: 8.74, UU: 1234 },
-    { date: "2024-09-27", PV: 1456, CV: 89, CVR: 6.11, UU: 1123 },
-    { date: "2024-09-28", PV: 1723, CV: 145, CVR: 8.41, UU: 1432 },
-    { date: "2024-09-29", PV: 1589, CV: 102, CVR: 6.42, UU: 1321 },
-    { date: "2024-09-30", PV: 1498, CV: 76, CVR: 5.07, UU: 1210 },
-  ];
-
   const sampledata2 = [
     { date: "2024-09-01", PV: 0, CV: 102, CVR: 6.42, UU: 1321 },
     { date: "2024-09-02", PV: 1498, CV: 76, CVR: 5.07, UU: 1210 },
@@ -302,7 +160,18 @@ const Dashboard = () => {
   const [endDate, setEndDate] = useState(
     new Date().toISOString().split("T")[0]
   );
+  const [startDate7, setStartDate7] = useState(() => {
+    const date7 = new Date();
+    date7.setDate(date7.getDate() - 7);
+    console.log("SDate7:", date7.toISOString().split("T")[0]);
+    const formattedDate = date7.toISOString().split("T")[0];
+    console.log("Formatted Date:", formattedDate);
+    return formattedDate;
+  });
   const [aggregatedData, setAggregatedData] = useState({});
+  const [aggregatedData7, setAggregatedData7] = useState({});
+  const [aggregatedData28, setAggregatedData28] = useState({});
+  const [aggregatedData90, setAggregatedData90] = useState({});
   /////////////////////////////////////////////////////////////////////////////////////////////////////////////
   const [propertyId, setPropertyId] = useState(null);
   const [chartData, setChartData] = useState([]);
@@ -322,7 +191,6 @@ const Dashboard = () => {
   const router = useRouter();
 
   const [sourceChartData, setSourceChartData] = useState([]);
-
   //Loadingの設定
   if (loading) {
     return <div>Loading...</div>;
@@ -491,6 +359,8 @@ const Dashboard = () => {
     if (fetchedAggregatedData) {
       console.log("Aggregated Data: ", fetchedAggregatedData);
       setAggregatedData(fetchedAggregatedData);
+      fetchAggregatedData7();
+      console.log("Aggregated Data:", aggregatedData);
     }
   }, [
     session,
@@ -501,6 +371,55 @@ const Dashboard = () => {
     aggregatedLoading,
     refetchAggregatedData,
   ]);
+
+  // 集計データを取得
+  const {
+    data: fetchedAggregatedData7,
+    error: aggregatedError7,
+    isLoading: aggregatedLoading7,
+    refetch: refetchAggregatedData7,
+  } = useAggregatedData(session, propertyIds, startDate7, endDate);
+
+  const fetchAggregatedData7 = () => {
+    console.log("session:", session);
+    console.log("propertyIds:", propertyIds);
+    console.log("startDate7:", startDate7);
+    console.log("endDate:", endDate);
+    if (
+      !session ||
+      !propertyIds.length ||
+      !startDate7 ||
+      !endDate ||
+      aggregatedLoading7
+    )
+      return; // session, propertyIds, startDate
+    if (aggregatedError7) {
+      console.error("Error fetching aggregated data:", aggregatedError7);
+      refetchAggregatedData7(session, propertyIds, startDate7, endDate); // エラー時にリフェッチ
+    }
+
+    if (fetchedAggregatedData7) {
+      console.log("Aggregated Data7: ", fetchedAggregatedData7);
+      console.log("SDate7:", startDate7);
+      console.log("EDate7:", endDate);
+      setAggregatedData7(fetchedAggregatedData7);
+    }
+  };
+
+  {
+    /*const handleAggregatedChange = () => {
+    if (!session || !propertyIds || !startDate || !endDate) return;
+
+    const data = refetchAggregatedData(
+      session,
+      propertyIds,
+      startDate,
+      endDate
+    );
+    console.log("Aggregated Data 7:", data);
+    setAggregatedData7(data);
+  };*/
+  }
 
   // フォーム送信時の処理
   const handleSubmit = (e) => {
@@ -1148,35 +1067,7 @@ const Dashboard = () => {
               </div>*/}
             </div>
           </div>
-          <div className="dashboard-top-right">
-            <div className="suggest-space"></div>
-          </div>
-        </div>
-        <div className="web-metrics-chart">
-          {/*<WebMetricsChart />*/}
-          <div className="triangle-chart-container">
-            <Trialgle
-              wPercent={65}
-              hPercent={25}
-              direction="right"
-              color="#4682b4"
-            />
-            <Trialgle
-              wPercent={40}
-              hPercent={15}
-              direction="right"
-              color="#1e90ff"
-            />
-            <Trialgle
-              wPercent={20}
-              hPercent={8}
-              direction="right"
-              color="#87cefa"
-            />
-            <div className="triangle-chart-text1 ">検索表示回数</div>
-            <div className="triangle-chart-text2">クリック数</div>
-            <div className="triangle-chart-text3">お問合せ数</div>
-          </div>
+          <div className="dashboard-top-right"></div>
         </div>
         <div className="dashboard-middle">
           <div className="dashboard-middle-content">
